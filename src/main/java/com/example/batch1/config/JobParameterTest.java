af2bebec7aa6b15f409767604e7d326f91f9a5d1
YuYoHan <zxzz8014@naver.com>
2024-01-14 22:22:39 +0900

    [Study] Batch

--- src/main/java/com/example/batch1/config/JobParameterTest.java
+++ src/main/java/com/example/batch1/config/JobParameterTest.java
@@ -9,18 +9,21 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-@Component
-public class JobRunner implements ApplicationRunner {
+import java.util.Date;
 
+@Component
+@RequiredArgsConstructor
+public class JobParameterTest implements ApplicationRunner {
     private final JobLauncher jobLauncher;
     private final Job job;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        JobParameters jobParameters =
-                new JobParametersBuilder().addString("name", "user2")
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user1")
+                .addLong("seq", 2L)
+                .addDate("date", new Date())
+                .addDouble("age", 16.5)
                 .toJobParameters();
 
         jobLauncher.run(job, jobParameters);

2a5cc82193035aebfe8ce35b6f1de21214f78f15
YuYoHan <zxzz8014@naver.com>
2025-03-04 20:29:58 +0900

    [Add] HelloJobConfig 추가

--- src/main/java/com/example/batch_sty/config/job/HelloJobConfig.java
+++ src/main/java/com/example/batch_sty/config/job/HelloJobConfig.java
@@ -1,7 +1,7 @@
-package com.example.batch_sty.config;
-
+package com.example.batch_sty.config.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -10,30 +10,25 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// 하나의 배치Job을 정의하고 빈 설정
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class HelloJobConfig {
-    // Job을 생성하는 빌더 팩토리
-    private final JobBuilderFactory jobBuilderFactory;
-    // Step을 생성하는 빌더 팩토리
+    private final JobBuilderFactory  jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    // helloJob 이름으로 Job 생성
-    @Bean
+    @Bean(name = "helloJob")
     public Job helloJob() {
         return jobBuilderFactory.get("helloJob")
                 .start(helloStep())
                 .build();
     }
 
-    // helloJob 이름으로 Step 생성
-    @Bean
+    @Bean(name = "helloStep")
     public Step helloStep() {
-        return stepBuilderFactory.get("hellostep")
-                // Step 안에서 단일 태스크로 수행되는 로직 구현
+        return stepBuilderFactory.get("helloStep")
                 .tasklet(((contribution, chunkContext) -> {
-                    System.out.println("hello spring batch");
+                    log.debug("hello Spring Batch");
                     return RepeatStatus.FINISHED;
                 })).build();
     }

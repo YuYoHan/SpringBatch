ec69a0eb290b6c2f3d83262e667ed112ac4a252f
YuYoHan <zxzz8014@naver.com>
2025-03-17 20:12:24 +0900

    [Study] 스케쥴러 & API로 디비 저장

--- src/main/java/com/example/batch_sty/BatchStyApplication.java
+++ src/main/java/com/example/batch_sty/BatchStyApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 // 스프링 배치가 작동하기 위해 선언해야 하는 어노테이션
 @EnableBatchProcessing
+@EnableScheduling
 public class BatchStyApplication {
 
     public static void main(String[] args) {
